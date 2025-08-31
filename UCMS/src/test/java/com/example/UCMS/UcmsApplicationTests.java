08ae71522419b1a929bb193ad5a3b317be2fa4d0
rhl728 <rahalpm@gmail.com>
2025-08-31 17:32:07 +0530

    created

---
+++ UCMS/src/test/java/com/example/UCMS/UcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
